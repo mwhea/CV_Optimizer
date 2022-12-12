1f2e15382bc3ea60bf712754e247ba52bd247196
mwhea <michael_wheatley@hotmail.com>
2022-12-12 12:57:36 -0500

    added test suite for sorting functionality

--- recursiveFunctions.test.js
+++ recursiveFunctions.test.js
@@ -5,7 +5,9 @@ import {
     isCategory, 
     tabulate, 
     removeUnpromisingCategories, 
-    convertObjectsToStrings
+    convertObjectsToStrings,
+    sortSections,
+    adjustImportances
 } from './recursiveFunctions';
 
 let blueprint = {
@@ -37,22 +39,14 @@ let blueprint = {
     "Supercategory": {
         "Programming Languages": [
             {
-                "property": "value",
+                "property": "skill example",
                 "importance": "3",
                 "skills demonstrated": [
                     "Skill"
                 ]
             }
         ],
-        "Programming Languages": [
-            {
-                "property": "value",
-                "importance": "1",
-                "skills demonstrated": [
-                    "Skill"
-                ]
-            }
-        ]
+        "Database Engines": []
     },
     "Jobs": [
         {
@@ -72,11 +66,11 @@ let blueprint = {
             "position": "Bad Position",
             "duties": [
                 {
-                    "description": "Supported day-to-day operations of Syn Studio art school, including classroom set-up, course organization, maintenance of art and study supplies, as well as tracking student participation.",
-                    "importance": "2"
+                    "description": "duty1",
+                    "importance": "1"
                 },
                 {
-                    "description": "Prepared introductory information for newly enrolled local and international students.",
+                    "description": "duty2",
                     "importance": "2"
                 }
             ]
@@ -175,7 +169,7 @@ describe("Test category remover", () => {
 
 describe("Test obj -> string conversion", () => {
 
-    beforeAll(()=>{
+    beforeAll(() => {
         sample = JSON.parse(JSON.stringify(blueprint));
         convertObjectsToStrings(sample);
     });
@@ -183,20 +177,182 @@ describe("Test obj -> string conversion", () => {
     test('Objects in a top-level array get theirs skillists removed?', () => {
         expect(sample["Array of Objects"][0].importance).not.toBeDefined();
     });
- 
- });
+
+});
+
+describe("Test sorting", () => {
+
+    beforeEach(() => {
+        sample = JSON.parse(JSON.stringify(blueprint));
+    })
+    test('Are skills in a skill list sorted?', () => {
+        sample["Supercategory"]["Database Engines"] = [
+        {
+            "property": "ss1",
+            "importance": "1",
+            "skills demonstrated": [
+                "Skill"
+            ]
+        },
+        {
+            "property": "ss2",
+            "importance": "3",
+            "skills demonstrated": [
+                "Skill"
+            ]
+        }]
+        sortSections(sample);
+        expect(sample["Supercategory"]["Database Engines"][0]["property"]).not.toEqual("ss1");
+    });
+    test('Does it sort down the whole list?', () => {
+        sample["Supercategory"]["Database Engines"] = [
+            {
+                "property": "ss1",
+                "importance": "1",
+                "skills demonstrated": [
+                    "Skill"
+                ]
+            },
+            {
+                "property": "ss2",
+                "importance": "3",
+                "skills demonstrated": [
+                    "Skill"
+                ]
+            },            {
+                "property": "ss3",
+                "importance": "1",
+                "skills demonstrated": [
+                    "Skill"
+                ]
+            },
+            {
+                "property": "ss4",
+                "importance": "3",
+                "skills demonstrated": [
+                    "Skill"
+                ]
+            }]
+            sortSections(sample);
+        console.log(sample["Supercategory"]["Database Engines"][0]["property"]);
+        expect(sample["Supercategory"]["Database Engines"][1]["property"]).toEqual("ss4");
+    });
+    test('Does it account for score adjustments?', () => {
+        sample["Supercategory"]["Database Engines"] = [
+            {
+                "property": "ss1",
+                "importance": "2",
+                "skills demonstrated": [
+                    "Skill"
+                ]
+            },
+            {
+                "property": "ss2",
+                "importance": "3",
+                "skills demonstrated": [
+                    "Skill"
+                ]
+            }, {
+                "property": "ss3",
+                "importance": "1",
+                "skills demonstrated": [
+                    "Boosted Skill"
+                ]
+            }]
+        adjustImportances(sample, ["Boosted Skill"]);
+        
+        sortSections(sample);
+        console.log(sample["Supercategory"]["Database Engines"][0]["property"]);
+        expect(sample["Supercategory"]["Database Engines"][1]["property"]).toEqual("ss3");
+    });
+
+    test('Does the lack of "skills demonstrated" properties break the sort?', () => {
+        sample["Supercategory"]["Database Engines"] = [
+            {
+                "property": "ss1",
+                "importance": "2"
+            },
+            {
+                "property": "ss2",
+                "importance": "3",
+                "skills demonstrated": [
+                    "Skill"
+                ]
+            }]
+        
+        sortSections(sample);
+        expect(sample["Supercategory"]["Database Engines"][0]["property"]).toEqual("ss2");
+    });
+
+    test('Can sorting priority be hardcoded?', () => {
+        sample["Supercategory"]["Database Engines"] = [
+            {
+                "property": "ss1",
+                "importance": "3"
+            },
+            {
+                "property": "ss2",
+                "importance": "1",
+                "order":"-1"
+            }]
+        
+        sortSections(sample);
+        expect(sample["Supercategory"]["Database Engines"][0]["property"]).toEqual("ss2");
+    });
+
+    test('How about in the opposite direction? (positive integers)', () => {
+        sample["Supercategory"]["Database Engines"] = [
+            {
+                "property": "ss1",
+                "importance": "3",
+                "order":"1"
+            },
+            {
+                "property": "ss2",
+                "importance": "1"
+            }]
+        
+        sortSections(sample);
+        expect(sample["Supercategory"]["Database Engines"][0]["property"]).toEqual("ss2");
+    });
+
+    test('Are job duties sorted?', () => {
+        sortSections(sample);
+        expect(sample["Jobs"][1]["duties"][0]["description"]).toEqual("duty2");
+    });
+
+    test('Are categories sorted by score?', () => {
+
+        sample["Supercategory"]["Database Engines"] = [
+            {
+                "property": "ss1",
+                "importance": "3"
+            },
+            {
+                "property": "ss2",
+                "importance": "3"
+            }, {
+                "property": "ss3",
+                "importance": "3"
+            }]
+            sortSections(sample);
+        expect(Object.keys(sample["Supercategory"])[0]).toEqual("Database Engines");
+    });
+
+});
+
 
 function whySupercategory(obj) {
-        return obj.reduce((errorString, o) => {
-            if (isCategory(o)) {
-                errorString += ((o.importance + ", " + o.duties + ", " + tabulate(o)) + '\n')
-            }
-            return errorString;
-        }, "")
-    }
+    return obj.reduce((errorString, o) => {
+        if (isCategory(o)) {
+            errorString += ((o.importance + ", " + o.duties + ", " + tabulate(o)) + '\n')
+        }
+        return errorString;
+    }, "")
+}
 
 function whyCategory(obj) {
 
-        console.log(`${obj.importance === undefined && tabulate(obj) > 0} || ${(obj.duties === undefined && tabulate(obj) > 0)}`)
+    console.log(`${obj.importance === undefined && tabulate(obj) > 0} || ${(obj.duties === undefined && tabulate(obj) > 0)}`)
 
-    }
+}
